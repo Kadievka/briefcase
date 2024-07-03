c6d82708382804cacc148c52e934140b9b7ec8e4
Kadievka Salcedo <kadievka.s.ks@gmail.com>
2024-07-03 12:32:32 +0200

    this is a commit to test 1

--- .jest/setEnvVarsExample.js
+++ .jest/setEnvVarsExample.js
@@ -1,5 +1,4 @@
-process.env.MONGODB_CONNECTION_STRING =
-    'mongodb://userExample:passwordExample@localhost:27017/';
+process.env.MONGODB_CONNECTION_STRING = 'mongodb://userExample:passwordExample@localhost:27017/';
 process.env.MONGODB_DB_NAME = 'Briefcase_Unit_Tests';
 process.env.PROTOCOL_URL='http://'
 process.env.BASE_URL='localhost:'
